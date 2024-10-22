f435b867c99e201bb6dd3f34b554b3a317976045
stefina <stefmai@hotmail.it>
2024-10-22 23:01:00 +0200

    modifica layout mobile

--- src/components/Contatti.jsx
+++ src/components/Contatti.jsx
@@ -1,6 +1,6 @@
 function Contatti() {
     return (
-        <div className="flex flex-col items-center justify-center text-center m-60">
+        <div className="flex flex-col items-center justify-center text-center mx-auto m-4 md:m-60">
             <h1 className="text-custom font-bold font-nunito text-4xl mb-4">Contatti</h1>
             <p className="text-lg">
                 Per qualsiasi tipo di segnalazione o per informazioni, scrivete al seguente indirizzo email: 
@@ -11,3 +11,4 @@ function Contatti() {
 }
 
 export default Contatti;
+
