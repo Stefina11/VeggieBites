636e65a0f0133c71cf709e6cfbdec7016fea2e3d
stefina <stefmai@hotmail.it>
2024-10-22 23:04:40 +0200

    modifica contatti

--- src/components/Contatti.jsx
+++ src/components/Contatti.jsx
@@ -1,6 +1,6 @@
 function Contatti() {
     return (
-        <div className="flex flex-col items-center justify-center text-center mx-auto m-4 md:m-60">
+        <div className="flex flex-col items-center justify-center text-center mx-auto m-4 md:m-60 mt-16 sm:mt-8">
             <h1 className="text-custom font-bold font-nunito text-4xl mb-4">Contatti</h1>
             <p className="text-lg">
                 Per qualsiasi tipo di segnalazione o per informazioni, scrivete al seguente indirizzo email: 
